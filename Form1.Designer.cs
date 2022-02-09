2b0e8ab95a476c59d91150fda5d16016869e4b9e
Ngounou Victor Perez <vngounou26@gmail.com>
2022-02-09 12:40:24 +0100

    Interface d'acceuil Creation de boutton Rond

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnFin = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnDebut = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.txtId);
+            this.panel1.Controls.Add(this.btnClose);
+            this.panel1.Controls.Add(this.btnLogin);
+            this.panel1.Controls.Add(this.btnDebut);
+            this.panel1.Controls.Add(this.btnFin);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1189, 601);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnFin
+            // 
+            this.btnFin.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnFin.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnFin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFin.ForeColor = System.Drawing.Color.White;
+            this.btnFin.Location = new System.Drawing.Point(665, 413);
+            this.btnFin.Name = "btnFin";
+            this.btnFin.Size = new System.Drawing.Size(176, 176);
+            this.btnFin.TabIndex = 0;
+            this.btnFin.Text = "Fin";
+            this.btnFin.UseVisualStyleBackColor = false;
+            this.btnFin.Paint += new System.Windows.Forms.PaintEventHandler(this.btnFin_Paint);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(66, 22);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(113, 113);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Paint += new System.Windows.Forms.PaintEventHandler(this.btnLogin_Paint);
+            // 
+            // btnDebut
+            // 
+            this.btnDebut.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnDebut.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnDebut.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDebut.ForeColor = System.Drawing.Color.White;
+            this.btnDebut.Location = new System.Drawing.Point(340, 413);
+            this.btnDebut.Name = "btnDebut";
+            this.btnDebut.Size = new System.Drawing.Size(176, 176);
+            this.btnDebut.TabIndex = 0;
+            this.btnDebut.Text = "Debut";
+            this.btnDebut.UseVisualStyleBackColor = false;
+            this.btnDebut.Paint += new System.Windows.Forms.PaintEventHandler(this.btnDebut_Paint);
+            // 
+            // txtId
+            // 
+            this.txtId.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtId.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtId.Location = new System.Drawing.Point(340, 352);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(501, 38);
+            this.txtId.TabIndex = 1;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(1064, 22);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(113, 113);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            this.btnClose.Paint += new System.Windows.Forms.PaintEventHandler(this.btnClose_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1189, 601);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnFin;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnDebut;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,56 @@ namespace Gestion_des_Vacataires
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void btnFin_Paint(object sender, PaintEventArgs e)
+        {
+            RondButton(btnFin, e);
+        }
+
+        private void btnDebut_Paint(object sender, PaintEventArgs e)
+        {
+            RondButton(btnDebut, e);
+        }
+
+        #region Mes Functions
+        /// <summary>
+        /// Donner la forme ronde à un Button
+        /// </summary>
+        /// <param name="button"></param>
+        /// <param name="e"></param>
+        public void RondButton(Button button,PaintEventArgs e)
+        {
+            GraphicsPath graphicsPath = new GraphicsPath();
+
+            Rectangle rectangle = button.ClientRectangle;
+
+            rectangle.Inflate(-10, -10);
+            e.Graphics.DrawEllipse(Pens.Black, rectangle);
+
+            rectangle.Inflate(1, 1);
+
+            graphicsPath.AddEllipse(rectangle);
+
+            button.Region = new Region(graphicsPath);
+            
+        }
+        #endregion
+
+        private void btnClose_Paint(object sender, PaintEventArgs e)
+        {
+            RondButton((Button)sender, e);
+        }
+
+        private void btnLogin_Paint(object sender, PaintEventArgs e)
+        {
+            RondButton((Button)(sender), e);
         }
     }
 }
